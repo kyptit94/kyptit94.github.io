55f89efdba478ebfc074723a581d7805c596abad
Pham Cao Ky <kyptit94@gmail.com>
2024-10-08 09:04:39 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/kyptit94/fb_vnm/' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/kyptit94/kyptit94.github.io/' : '/'
 }
